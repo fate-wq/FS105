1f5a561d299e845dd9b99ea2abb9c8e7906794df
Eunice <sofox.kaka@gmail.com>
2024-07-02 15:56:34 +0800

    completed most of the layout in CSS format

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,31 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login', { title: 'Login' });
+});
+
+app.get('/loginUser', (req, res) => {
+    res.render('loginUser', { title: 'Login User' });
+});
+
+
+app.get('/createUserProfile', (req, res) => {
+    res.render('createUserProfile', { title: 'Create Profile' });
+});
+
+app.get('/createEMProfile', (req, res) => {
+    res.render('createEMProfile', { title: 'Create Employer Profile' });
+});
+
+app.get('/updateProfile', (req, res) => {
+    res.render('updateProfile', { title: 'Update Profile' });
+});
+
+app.get('/jobPosting', (req, res) => {
+    res.render('jobPosting', { title: 'Job Posting' });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
